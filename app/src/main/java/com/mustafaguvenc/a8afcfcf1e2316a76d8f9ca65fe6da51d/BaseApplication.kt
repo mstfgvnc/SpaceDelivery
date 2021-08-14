6adc09ffbe73d9abd16c24101f5818eaf71b592c
mstfgvnc <mustafa_gvnc@windowslve.com>
2021-08-14 11:33:58 +0300

    Update

--- app/src/main/java/com/mustafaguvenc/a8afcfcf1e2316a76d8f9ca65fe6da51d/BaseApplication.kt
+++ app/src/main/java/com/mustafaguvenc/a8afcfcf1e2316a76d8f9ca65fe6da51d/BaseApplication.kt
@@ -3,6 +3,7 @@ package com.mustafaguvenc.a8afcfcf1e2316a76d8f9ca65fe6da51d
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+// Hilt başlangıcı
 @HiltAndroidApp
 class BaseApplication (): Application() {
 }
